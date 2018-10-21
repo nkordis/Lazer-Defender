a40cedefcb76eba5a154c520c330dc01782b6e27
Nikos Kordis <nikoskordis@gmail.com>
2018-10-21 16:55:33 +0200

    Delay Game Over scene

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class Level : MonoBehaviour {
 
+    float delayInSeconds = 2f;
+
+
 	public void LoadStartMenu()
     {
         SceneManager.LoadScene(0);
@@ -17,6 +20,13 @@ public class Level : MonoBehaviour {
 
     public void LoadGameOver()
     {
+        StartCoroutine("DelayGameOver");
+        
+    }
+
+    IEnumerator DelayGameOver()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
         SceneManager.LoadScene("Game Over");
     }
 
